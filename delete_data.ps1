1f65e8f47eb551016bbc42eed587bd73c213be2c
Omar <omarfarouk219@gmail.com>
2025-01-21 03:38:05 +0200

    complete the setup by adding standby, replication user, and delete data scripts

---
+++ delete_data.ps1
@@ -0,0 +1,7 @@
+Remove-Item -Path ".\master" -Recurse -Force
+
+Remove-Item -Path ".\standby1" -Recurse -Force
+
+Remove-Item -Path ".\standby2" -Recurse -Force
+
+Remove-Item -Path ".\standby3" -Recurse -Force
